0bb77ea500a29306c54d51673d41370bf7bbc3bd
Marco Zollinger <marco@freelabs.space>
2019-03-20 00:31:28 +0100

    basic text and file serving capability over HTTP

--- src/main/java/ch/hsr/epj/localshare/transferprototype/Main.java
+++ src/main/java/ch/hsr/epj/localshare/transferprototype/Main.java
@@ -1,7 +1,58 @@
 package ch.hsr.epj.localshare.transferprototype;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.Headers;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("new project");
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8640), 0);
+        server.createContext("/info", new InfoHandler());
+        server.createContext("/get", new GetHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        System.out.println("HTTP server started");
+    }
+
+    static class InfoHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Use /get to download a PDF";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            System.out.println("/info was requested");
+            os.close();
+        }
+    }
+
+    static class GetHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+
+            // add the required response header for a PDF file
+            Headers h = t.getResponseHeaders();
+            h.add("Content-Type", "application/pdf");
+
+            // a PDF (you provide your own!)
+            File file = new File("c:/tmp/test.pdf");
+            byte[] bytearray = new byte[(int) file.length()];
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            bis.read(bytearray, 0, bytearray.length);
+
+            // ok, we are ready to send the response.
+            t.sendResponseHeaders(200, file.length());
+            OutputStream os = t.getResponseBody();
+            os.write(bytearray, 0, bytearray.length);
+            System.out.println("/get was requested");
+            os.close();
+        }
     }
 }
