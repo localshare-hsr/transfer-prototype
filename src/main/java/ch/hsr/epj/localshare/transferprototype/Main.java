0cc1dd60453007afd35121e3563513e9b0177640
Marco Zollinger <marco@freelabs.space>
2019-03-20 00:57:25 +0100

    try a relative file path

--- src/main/java/ch/hsr/epj/localshare/transferprototype/Main.java
+++ src/main/java/ch/hsr/epj/localshare/transferprototype/Main.java
@@ -40,14 +40,14 @@ public class Main {
             Headers h = t.getResponseHeaders();
             h.add("Content-Type", "application/pdf");
 
-            // a PDF (you provide your own!)
-            File file = new File("c:/tmp/test.pdf");
+            // serve the PDF
+            File file = new File("test.pdf");
             byte[] bytearray = new byte[(int) file.length()];
             FileInputStream fis = new FileInputStream(file);
             BufferedInputStream bis = new BufferedInputStream(fis);
             bis.read(bytearray, 0, bytearray.length);
 
-            // ok, we are ready to send the response.
+            // send the response
             t.sendResponseHeaders(200, file.length());
             OutputStream os = t.getResponseBody();
             os.write(bytearray, 0, bytearray.length);
